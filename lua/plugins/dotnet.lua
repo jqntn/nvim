fbabbb6a05c15921189fc4b3a1556a4c4322463e
Julien QUENTIN <jqntn88@gmail.com>
2026-09-03 11:38:09 +0200

    perf(lsp): limit roslyn_ls diagnostics to open files

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -5,7 +5,14 @@ return {
     opts = {
       servers = {
         omnisharp = { enabled = false },
-        roslyn_ls = {},
+        roslyn_ls = {
+          settings = {
+            ["csharp|background_analysis"] = {
+              dotnet_analyzer_diagnostics_scope = "openFiles",
+              dotnet_compiler_diagnostics_scope = "openFiles",
+            },
+          },
+        },
       },
     },
   },
